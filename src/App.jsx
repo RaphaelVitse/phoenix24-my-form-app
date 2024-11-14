9889050cae6bf0dcff85b11450f061388edc005a
Raphael Vitse <raphaelv@MacBook-Pro-de-raphael.local>
2024-11-14 15:37:43 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import Footer from "./components/Footer";
+
+import "./App.css";
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
+  const [showError, setShowError] = useState(false);
+  const [resumeInfos, setResumeInfos] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setShowError(true);
+    } else if (password === confirmPassword) {
+      setResumeInfos(true);
+    }
+  };
+
+  return (
+    <>
+      <main className="container">
+        <form className={resumeInfos && "hidden"} onSubmit={handleSubmit}>
+          <h2> Create Acccount</h2>
+          <div>
+            <p>Name</p>
+            <input
+              type="text"
+              placeholder="John Doe"
+              value={username}
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
+            />
+            <p>Email</p>
+            <input
+              type="email"
+              placeholder="johndoe@gmail.com"
+              value={email}
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+            />
+            <p>Password</p>
+            <input
+              className={showError && "error"}
+              type="password"
+              placeholder="Ffkge23h85fT"
+              value={password}
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+            <p>Confirm your password</p>
+            <input
+              className={showError && "error"}
+              type="password"
+              placeholder="Ffkge23h85fT"
+              value={confirmPassword}
+              onChange={(event) => {
+                setconfirmPassword(event.target.value);
+              }}
+            />
+            <p className="errorPassword">
+              {showError && "Les mots de passe ne sont pas identiques"}
+            </p>
+            <button type="submit">Register</button>
+          </div>
+        </form>
+
+        <div className={resumeInfos ? "show" : "hidden"}>
+          <div className="resumeInfos">
+            <div>
+              <h2>Result</h2>
+              <p>Name : {username}</p>
+              <p>Email : {email}</p>
+              <p>Password : {password}</p>
+              <button type="submit" onClick={() => setResumeInfos(false)}>
+                Edit your information
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <Footer />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+const Form = (props) => {
+  return (
+    <>
+      <footer>
+        <p>
+          Made with <span> React </span> at <span> Le Reacteur </span> by
+          <span> Raphaël </span>
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Form;
